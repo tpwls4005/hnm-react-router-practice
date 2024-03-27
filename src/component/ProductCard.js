73f344d4f94a53d77ec90bfc0b06e4a3973facc5
sejin min <tpwls4005@naver.com>
2024-03-28 02:28:25 +0900

    first commit

---
+++ src/component/ProductCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ProductCard =()=>{
+    return(
+    <div>
+        <img src="//lp2.hm.com/hmgoepprod?set=source[/45/81/458112e4714d91ccd077783c2ab31da3d1c49690.jpg],origin[dam],category[],type[LOOKBOOK],res[z],hmver[1]&call=url[file:/product/main]"/>
+        <div>파인니트 티셔츠</div>
+        <div>24,000</div>
+        <div>신제품</div>
+    </div>
+    );
+};
+
+export default ProductCard;
---
+++ src/page/ProductDetail.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ProductDetail =()=>{
+    return(
+    <div>상품</div>
+    );
+};
+
+export default ProductDetail;
